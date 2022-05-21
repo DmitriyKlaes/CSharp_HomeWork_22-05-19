cd37fc4883110a5688e86780c8219c6c3e8a54ac
Dmitriy_Klaes <89119584059@mail.ru>
2022-05-21 22:33:37 +0300

    task000-PowerOf

---
+++ task000-PowerOf/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int PowerOf(int number, int power)
+{
+    int result = 1;
+    for (int i = 0; i < power; i++)
+    {
+        result = result * number;
+    }
+    return result;
+}
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите степень числа: ");
+int power = Convert.ToInt32(Console.ReadLine());
+int result = PowerOf(number, power);
+System.Console.WriteLine($"{number}^{power} = {result}");
