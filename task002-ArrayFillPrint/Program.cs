7190b0c73af035b9c94993f9c624836781cc4e48
Dmitriy_Klaes <89119584059@mail.ru>
2022-05-22 21:56:07 +0300

    task002 подкорректировал под условие задачи

--- task002-ArrayFillPrint/Program.cs
+++ task002-ArrayFillPrint/Program.cs
@@ -4,26 +4,14 @@
 6, 1, 33 -> [6, 1, 33]
 */
 
-void FillArray(int[] array, string arg)
+//Написал код исходя из условий задачи. Примеры результата из задания не подходят под условие задачи.
+//Более гибкая версия программы под номером task002-1.
+
+void FillArray(int[] array)
 {
-    if (arg.ToLower() == "y")
+    for (int i = 0; i < array.Length; i++)
     {
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i] = new Random().Next(1, 100);
-        }
-    }
-    else if (arg.ToLower() == "n")
-    {
-        Console.WriteLine("Введите 8 чисел через Enter: ");
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i] = Convert.ToInt32(Console.ReadLine());
-        }
-    }
-    else
-    {
-        Console.WriteLine("Неверный выбор, попробуйте ещё раз!");
+        array[i] = Convert.ToInt32(Console.ReadLine());
     }
 }
 void PrintArray(int[] array)
@@ -34,48 +22,12 @@ void PrintArray(int[] array)
         Console.Write(array[i]);
         if (i < array.Length - 1)
         {
-            Console.Write(",");
+            Console.Write(", ");
         }
     }
     Console.Write("]");
 }
 int[] array = new int[8];
-Console.WriteLine("Выбрать случайные 8 чисел от 1 до 99 (ввелите Y или N)?: ");
-string choice = Console.ReadLine()!;
-FillArray(array, choice);
-if (choice.ToLower() == "n" || choice.ToLower() == "y")
-{
-    PrintArray(array);
-}
-/*
-else
-{
-    return;
-}
-*/
-
-/*
-while (choice.ToLower() == "y")
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(1, 100);
-    }
-    break;
-}
-while (choice.ToLower() == "n")
-{
-    Console.WriteLine("Введите 9 случайных чисел через (Пробел):");
-    string[] arrayNumber = (Console.ReadLine()!.Split(' '));
-    for (int i = 0; i < arrayNumber.Length; i++)
-    {
-        array[i] = int.Parse(arrayNumber[i]);
-    }
-    break;
-}
-int max = Max(Max(array[0], array[1], array[2]),
-              Max(array[3], array[4], array[5]),
-              Max(array[6], array[7], array[8]));
-string str = string.Join(", ", array);
-Console.WriteLine($"Максимальным из чисел ({str}) является {max}");
-*/
+Console.WriteLine("Введите 8 чисел через Enter: ");
+FillArray(array);
+PrintArray(array);
